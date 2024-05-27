2d65579a2558e34f00c1583242a7e87d3ad19dd0
Johan Loke <johanloke@gmail.com>
2024-05-27 15:33:55 +0800

    update

--- demo/postgres/change.sql
+++ demo/postgres/change.sql
@@ -10,3 +10,17 @@ ALTER TABLE public.student REPLICA IDENTITY DEFAULT;
 
 INSERT INTO public.student ("name") VALUES
     ('Ali');
+
+INSERT INTO public.student ("name") values ('Ali');
+
+UPDATE public.student SET "name"='Ali' WHERE id=1;
+
+UPDATE public.student SET "name"='Alibaba' WHERE id=1;
+
+INSERT INTO public.student ("name") values ('Muthu');
+
+UPDATE public.student SET "name"='Muthu' WHERE id=2;
+
+INSERT INTO public.student ("name") values ('Ah Kao');
+
+UPDATE public.student SET "name"='Ah kao' WHERE id=3;
